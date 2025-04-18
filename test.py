3659fe1cbb081d10fb993b70f500c5880c032b6d
web_server <duanjintao@kuaishou.com>
2025-04-18 15:33:49 +0800

    nginx

---
+++ test.py
@@ -0,0 +1,11 @@
+import re
+
+COMMON_TYPOS = (
+    (r'^https://(.*?.youtube.com)/', r'https://siteproxy.fcing.email/user22334455/https/\1/'),
+    # (r'^https://', r'httpss://'),
+)
+url = "https://www.youtube.com/watch?v=rTSI-pUWgF0";
+for mistake, fixup in COMMON_TYPOS:
+    if re.match(mistake, url):
+        print(re.sub(mistake, fixup, url))
+print(url)
