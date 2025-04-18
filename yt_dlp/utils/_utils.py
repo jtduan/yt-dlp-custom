3c374b1d1fefe309d481f0c86b9fd5330681629a
web_server <duanjintao@kuaishou.com>
2025-04-18 16:35:39 +0800

    debug m3u8

--- yt_dlp/utils/_utils.py
+++ yt_dlp/utils/_utils.py
@@ -746,6 +746,7 @@ def sanitize_url(url, *, scheme='http'):
         # https://bx1.be/lives/direct-tv/
         # (r'^rmtp([es]?)://', r'rtmp\1://'),
     )
+    print(url)
     for mistake, fixup in COMMON_TYPOS:
         if re.match(mistake, url):
             return re.sub(mistake, fixup, url)
